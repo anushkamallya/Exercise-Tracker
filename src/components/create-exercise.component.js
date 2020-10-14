a63e573b2b50613263d4d8cbc2a23e1aa2fc6662
amallya967 <anushka.mallya@gmail.com>
2020-10-14 18:28:35 -0500

    Update components

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,10 +1,133 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class CreateUser extends Component {
+export default class CreateExercise extends Component {
+  //Call super when declaring constructor of a subclass
+  constructor(props) {
+    super(props);
+
+    //To ensure 'this' works properly, we have to bind the methods to 'this'
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    //Properties of state correspond to the fields in the MongoDB document
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+
+  //This method is a part of the React life cycle and is invoked immediately after a component is mounted
+  componentDidMount() {
+    this.setState({ 
+      users: ['test user'],
+      username: 'test user'
+    });
+  }
+
+  //Add methods which can be used to update the state properties
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    //preventDefault prevents the default HTML form submit from taking place
+    e.preventDefault();
+  
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+  
+    console.log(exercise);
+    
+    window.location = '/';
+  }
+  
+  //The form code is placed in render because that is what needs to be displayed
   render() {
     return (
       <div>
-        <p>You are on the Create User component!</p>
+        <h3>Create New Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <select ref="userInput"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}>
+                {
+                  this.state.users.map(function(user) {
+                    return <option 
+                      key={user}
+                      value={user}>{user}
+                      </option>;
+                  })
+                }
+            </select>
+          </div>
+          <div className="form-group"> 
+            <label>Description: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input 
+                type="text" 
+                className="form-control"
+                value={this.state.duration}
+                onChange={this.onChangeDuration}
+                />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
     )
   }
